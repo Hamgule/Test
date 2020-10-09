280c102dcaf0c05f9dbc7b0702f284a7615ded1e
Hamgule <hsj6831@naver.com>
2020-10-09 23:31:56 +0900

    Test2

--- test.java
+++ test.java
@@ -1,6 +1,15 @@
-class Test() {
-    public static void main(STring[] args) {
-        System.out.println("Hello, World!");
+import javax.swing.*;
+
+class Test {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Test");
+
+        JLabel label = new JLabel("Hello, World!");
+        frame.add(label);
+
+        frame.setSize(new Dimension(480, 320));
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
 
